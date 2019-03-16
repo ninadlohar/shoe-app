64eaa9deef66c27c29eb375c5124e6bf40dad2a4
ninadlohar <ninad.lohar94@gmail.com>
2019-03-16 11:38:07 +0530

    fixed small buttons and added size section

--- src/MainContainer/WebPageContents/InputBox/InputBox.js
+++ src/MainContainer/WebPageContents/InputBox/InputBox.js
@@ -1,15 +1,57 @@
 import React from "react";
-import { Form, Input } from "reactstrap";
+import {
+  Form,
+  Input,
+  ButtonDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
 
-const InputBox = () => {
-  return (
-    <div className="inputBox">
-      <Form>
-        <Input type="text" name="text" id="inputText" placeholder="Search" />
-        <i className="fas fa-search" />
-      </Form>
-    </div>
-  );
-};
+class InputBox extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
+  render() {
+    return (
+      <div className="drop-Down">
+        <div>
+          <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+            <DropdownToggle caret>Sizes</DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem header>Header</DropdownItem>
+              <DropdownItem disabled>Action</DropdownItem>
+              <DropdownItem>Another Action</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Another Action</DropdownItem>
+            </DropdownMenu>
+          </ButtonDropdown>
+        </div>
+        <div className="inputBox">
+          <Form>
+            <Input
+              type="text"
+              name="text"
+              id="inputText"
+              placeholder="Search"
+            />
+            <i className="fas fa-search" />
+          </Form>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default InputBox;
