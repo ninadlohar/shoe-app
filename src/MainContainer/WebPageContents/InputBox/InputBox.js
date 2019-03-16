4763c93f385eab1bffa7454c01fb39e64fa37cfa
ninadlohar <ninad.lohar94@gmail.com>
2019-03-16 13:44:15 +0530

    fixed shoe size row issue

--- src/MainContainer/WebPageContents/InputBox/InputBox.js
+++ src/MainContainer/WebPageContents/InputBox/InputBox.js
@@ -30,11 +30,37 @@ class InputBox extends React.Component {
           <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
             <DropdownToggle caret>Sizes</DropdownToggle>
             <DropdownMenu>
-              <DropdownItem header>Header</DropdownItem>
-              <DropdownItem disabled>Action</DropdownItem>
-              <DropdownItem>Another Action</DropdownItem>
-              <DropdownItem divider />
-              <DropdownItem>Another Action</DropdownItem>
+              <DropdownItem className="show-hide-items">
+                Show/Hide Intermediate Sizes
+                <span className="show-size1">UK/</span>
+                <span className="show-size2">US/</span>
+                <span className="show-size3">UK</span>
+              </DropdownItem>
+              <DropdownItem>
+                <div className="size-list">
+                  <div className="s1">
+                    <div className="pd8">36</div>
+                    <div className="pd8">37</div>
+                    <div className="pd8">38</div>
+                    <div className="pd8">39</div>
+                    <div className="pd8">40</div>
+                    <div className="pd8">41</div>
+                  </div>
+                  <div className="s2">
+                    <div className="pd8">42</div>
+                    <div className="pd8">43</div>
+                    <div className="pd8">44</div>
+                    <div className="pd8">45</div>
+                    <div className="pd8">46</div>
+                    <div className="pd8">47</div>
+                  </div>
+                  <div className="s3">
+                    <div className="pd8">48</div>
+                    <div className="pd8">49</div>
+                    <div className="pd8">50</div>
+                  </div>
+                </div>
+              </DropdownItem>
             </DropdownMenu>
           </ButtonDropdown>
         </div>
