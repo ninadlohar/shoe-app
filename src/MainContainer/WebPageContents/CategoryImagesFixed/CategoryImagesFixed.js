00b842fa4ad0a081468eca5d701f1d3031ec4bcf
ninadlohar <ninad.lohar94@gmail.com>
2019-03-20 12:03:18 +0530

    added category shoes white box w/ animation on hover

--- src/MainContainer/WebPageContents/CategoryImagesFixed/CategoryImagesFixed.js
+++ src/MainContainer/WebPageContents/CategoryImagesFixed/CategoryImagesFixed.js
@@ -1,70 +1,90 @@
 import React from "react";
 import "../../global.scss";
-import { Container, Row, Col } from "reactstrap";
+import { Row, Col } from "reactstrap";
 
 class CategoryImagesFixed extends React.Component {
   render() {
     return (
       <div className="cat-images">
         <Col md="12" sm="12" lg="12" xl="12" sm="12">
-          <Row className="nomargin">
-            <Col
-              sm="12"
-              xs="12"
-              md="12"
-              lg="12"
-              xl="12"
-              className="al-it-center"
-            >
-              <img
-                src={require("../../../assets/cat1.png")}
-                alt="cat1"
-                className="img-responsive"
-              />
-            </Col>
-            <Col
-              sm="12"
-              xs="12"
-              md="12"
-              lg="12"
-              xl="12"
-              className="al-it-center"
-            >
-              <img
-                src={require("../../../assets/cat2.png")}
-                alt="cat2"
-                className="img-responsive"
-              />
-            </Col>
-            <Col
-              sm="12"
-              xs="12"
-              md="12"
-              lg="12"
-              xl="12"
-              className="al-it-center"
-            >
-              <img
-                src={require("../../../assets/cat3.png")}
-                alt="cat3"
-                className="img-responsive"
-              />
-            </Col>
+          <Row
+            className="nomargin"
+            style={{ justifyContent: "center", alignItems: "center" }}
+          >
+            <div>
+              <Col
+                sm="12"
+                xs="12"
+                md="12"
+                lg="12"
+                xl="12"
+                className="al-it-center"
+              >
+                <div className="cat-card">
+                  <img
+                    src={require("../../../assets/cat1.png")}
+                    alt="cat1"
+                    className="img-responsive"
+                  />
+                  <div className="cat-card-hover-clone">
+                    <div className="_shoes_white_box">
+                      <div className="_shoes">Kids Shoes</div>
+                    </div>
+                  </div>
+                </div>
+              </Col>
+            </div>
+            <div>
+              <Col
+                sm="12"
+                xs="12"
+                md="12"
+                lg="12"
+                xl="12"
+                className="al-it-center"
+              >
+                <div className="cat-card">
+                  <img
+                    src={require("../../../assets/cat2.png")}
+                    alt="cat2"
+                    className="img-responsive"
+                  />
+                  <div className="cat-card-hover-clone">
+                    <div className="_shoes_white_box">
+                      <div className="_shoes">Women Shoes</div>
+                    </div>
+                  </div>
+                </div>
+              </Col>
+            </div>
+            <div>
+              <Col
+                sm="12"
+                xs="12"
+                md="12"
+                lg="12"
+                xl="12"
+                className="al-it-center"
+              >
+                <div className="cat-card">
+                  <img
+                    src={require("../../../assets/cat3.png")}
+                    alt="cat3"
+                    className="img-responsive"
+                  />
+                  <div className="cat-card-hover-clone">
+                    <div className="_shoes_white_box">
+                      <div className="_shoes">Men Shoes</div>
+                    </div>
+                  </div>
+                </div>
+              </Col>
+            </div>
           </Row>
         </Col>
       </div>
     );
   }
 }
-
-// const CategoryImagesFixed = () => {
-//   return (
-//     // <Container fluid={true} className="nopadding">
-//     //   <Row className="nomargin">
-//     <
-//     //   </Row>
-//     // </Container>
-//   );
-// };
 
 export default CategoryImagesFixed;
